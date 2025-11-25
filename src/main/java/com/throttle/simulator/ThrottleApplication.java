5699e01370c124a2b0d008883bc17000307f20af
=Dheeraj Pali <=dheeraj.pali@atqor.com>
2025-11-25 11:28:13 +0530

    springboot application setup

---
+++ src/main/java/com/throttle/simulator/ThrottleApplication.java
@@ -0,0 +1,13 @@
+package com.throttle.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrottleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThrottleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/throttle/simulator/ThrottleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.throttle.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrottleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
